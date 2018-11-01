da6f56706653951b9fe0af3b0dc83ae55936c420
Filip Vitas <filip.vitas.dev@gmail.com>
2018-11-01 23:09:27 +0100

    initial commit for counter with parcel + babel 7

---
+++ counter/index.js
@@ -0,0 +1,30 @@
+import { render, WeElement, tag, observe } from 'omi'
+
+@observe
+@tag("my-counter")
+class MyApp extends WeElement {
+
+    data = {
+        count: 0
+    }
+
+    sub = () => {
+        this.data.count--
+    }
+
+    add = () => {
+        this.data.count++
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.sub}>-</button>
+                <span>{this.data.count}</span>
+                <button onClick={this.add}>+</button>
+            </div>
+        )
+    }
+}
+
+render(<my-counter />, 'body')
